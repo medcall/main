49d5d4859f53b35ec9910f03e7df0984c78c4f29
itsnoa04 <itsdevamshmanoj@gmail.com>
2022-06-18 16:01:55 +0530

    login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+ 
+const login = () => {
+  return (
+    <div className="login">
+      <div className="container">
+        <h1>Help of medical experts in just one click.</h1>
+ 
+        <div className="google_signIn_button"></div>
+      </div>
+    </div>
+  );
+};
+ 
+export default login;
